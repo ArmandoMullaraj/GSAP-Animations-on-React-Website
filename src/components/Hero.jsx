f12e53b31368e5128aaeb1af4abb4f2701261c2c
Armando Mullaraj <zerojuanalol@gmail.com>
2025-10-15 16:06:16 +0100

    added animation video on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,15 @@
 import { useGSAP } from '@gsap/react'
-import React from 'react'
 import gsap from 'gsap'
 import { SplitText } from 'gsap/all'
+import { useMediaQuery } from 'react-responsive'
+import { useRef } from 'react'
 
 const Hero = () => {
+
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+
     useGSAP(() => {
         const heroSplit = new SplitText('.title', { type: 'chars, words' });
         const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
@@ -25,36 +31,81 @@ const Hero = () => {
             stagger: 0.06,
             delay: 1,
         })
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: '#hero',
+                start: 'top top',
+                end: 'bottom top',
+                scrub: true,
+            }
+        })
+            .to('.right-leaf', { y: 200 }, 0)
+            .to('.left-leaf', { y: -200 }, 0)
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        //Video Scroll Animation
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+                // onEnter: () => videoRef.current.play(),
+                // onEnterBack: () => videoRef.current.play(),
+                // onLeave: () => videoRef.current.pause(),
+                // onLeaveBack: () => videoRef.current.pause(),
+            },
+        })
+
+        videoRef.current.addEventListener('loadedmetadata', () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            });
+        });
     }, []);
-  return (
-    <>
-    <section id="hero" className="noisy">
-        <h1 className="title">MOJITO</h1>
+    return (
+        <>
+            <section id="hero" className="noisy">
+                <h1 className="title">MOJITO</h1>
 
-        <img src="/images/hero-left-leaf.png"
-             alt="left-leaf"
-             className="left-leaf"
-        />
+                <img src="/images/hero-left-leaf.png"
+                    alt="left-leaf"
+                    className="left-leaf"
+                />
 
-        <img src="/images/hero-right-leaf.png"
-             alt="right-leaf"
-             className="right-leaf"
-        />
+                <img src="/images/hero-right-leaf.png"
+                    alt="right-leaf"
+                    className="right-leaf"
+                />
 
-        <div className="body">
-            <div className="content">
-                <div className="space-y-5 md:block">
-                    <p>Cool. Crisp. Classic.</p>
-                    <p className="subtitle">The Original <br /> Cuban Cocktail</p>
+                <div className="body">
+                    <div className="content">
+                        <div className="space-y-5 md:block">
+                            <p>Cool. Crisp. Classic.</p>
+                            <p className="subtitle">The Original <br /> Cuban Cocktail</p>
+                        </div>
+                        <div className="view-cocktails">
+                            <p className="subtitle">Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes - designed to delight your senses.</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="view-cocktails">
-                    <p className="subtitle">Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes - designed to delight your senses.</p>
-                </div>
+            </section>
+
+            <div className="video absolute inset-0">
+                <video
+                    ref={videoRef}
+                    src="/videos/output.mp4"
+                    muted
+                    playsInline
+                    preload="auto">
+                </video>
             </div>
-        </div>
-    </section>
-    </>
-  )
+        </>
+    )
 }
 
 export default Hero
